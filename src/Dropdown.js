1985136ef15a0f50980e60a2c1f579944cf234ca
Jairo Santos <jairo.tsantos@ufpe.br>
2022-11-23 17:29:42 -0300

    melhorias no menu

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,11 +1,13 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+
 
 const Dropdown = ({submenu, dropdown}) =>{
 
     return (
-        <ul className={`dropdown ${dropdown ? 'show' : ''}`}>
-            {console.log(`dropdown ${dropdown ? 'show' : ''}`)}
-            {submenu.map(sub =>(<li className="menu-items"><Link  to={sub.url}>{sub.titulo}</Link></li>))}
+        <ul className={`dropdown ${dropdown ? '' : 'notshow'}`}>
+            {console.log(`dropdown ${dropdown ? 'notshow' : ''}`)}
+             {submenu && submenu.map(sub =>(<li className="menu-items" key={sub.titulo}><Link  to={sub.url}>{sub.titulo}</Link></li>))}
         </ul>
     )
 }
--- src/MenuItens.js
+++ src/MenuItens.js
@@ -12,6 +12,24 @@ const MenuItens = ({itens}) =>{
     console.log('entrou');
      {<Dropdown submenu={itens.submenu}/>};
   }*/}
+//mecanismo para ocultar o 
+
+useEffect(()=>{
+  //setDropdown(dropdown)
+  console.log('entrou no effect')
+  if (!dropdown){
+    ocultar();
+    console.log('ocultou')
+  }
+},[dropdown]
+
+);
+
+  const ocultar = () =>{
+     setDropdown(dropdown? !dropdown : dropdown)
+     console.log('entrou no ocultar '+dropdown)
+  }
+
     return(
      
   <li className="menu-items" >
@@ -19,9 +37,13 @@ const MenuItens = ({itens}) =>{
                     <>
                         <button type="button" aria-haspopup="menu" aria-expanded={dropdown ? "true" :"false"} onClick={()=>setDropdown((prev)=>!prev)}>{itens.titulo}</button>
                         {console.log('o dropdown eh '+dropdown)}
-                        <Dropdown submenu={itens.submenu} dropdown={dropdown}/>
+                         <Dropdown submenu={itens.submenu} dropdown={dropdown}/>
                     </>  ) : 
-                           <Link to={itens.url} >{itens.titulo}</Link>}
+                           
+                           <Link to={itens.url} onClick={ocultar} >{itens.titulo}</Link>}
+                           {console.log('valor'+`dropdown ${dropdown ? '' : 'notshow'}`)}
+                           
+
   </li>
     )
 }
